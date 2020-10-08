8d04d49ada6359d2ae664d883a0c699249566f72
Panzarborg <71063257+Panzarborg@users.noreply.github.com>
2020-10-08 12:12:34 +0200

    asignment 5

---
+++ five/Main.java
@@ -0,0 +1,16 @@
+package five;
+
+public class Main {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+
+
+        name = new name();
+        age = new age();
+
+
+    }
+}
---
+++ five/car.java
@@ -0,0 +1,21 @@
+package five;
+
+public class car {
+
+    private String make;
+    private String model;
+    private int year;
+    private String bodyStyle;
+    private String driver;
+
+    public car(String make, String model, int year, String bodyStyle) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.bodyStyle = bodyStyle;
+
+    }
+}
+
+
+
